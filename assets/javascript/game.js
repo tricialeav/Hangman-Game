6a0f3b6533def65d34ae63ce2c2d57a161007da8
tricialeav <leavitt.tricia@gmail.com>
2018-02-06 23:24:14 -0800

    Added additional counters and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,17 @@
-var start; // var, let and const??
+// To figure out:
+// var, let and const - when is best to use each one?
+// add timeout to key press
+// set up letters in order for correct guesses (forEach?)
+// display and disable duplicate letters
+// how to move to next word if all correct
+
+var start;
 var lives = 10;
 var key;
 let correct;
 let incorrect;
+let guessRight = 0;
+let wordsGuessed = 0;
 const words = ["cabbage", "burrito", "cookies"];
 
 // user presses a button to start game
@@ -15,41 +24,41 @@ function run(evt) {
   document.getElementById("guessLeft").innerHTML = lives;
 
   // user presses key to guess a letter
-
+ 
   if (start === true) {
     document.body.addEventListener("keyup", userInput);
   }
 }
 
-function userInput(evt) {
-  key = event.key;
-  // compare input to hangman word
-  checkWord();
-  function checkWord() {
-    let currentWord = words[0].search(key);
-    console.log(currentWord);
-    if (currentWord < 0) {
-      document.getElementById("incorrect").innerHTML += " " + key;
-      lives -= 1;
-      document.getElementById("guessLeft").innerHTML = lives;
-    } else {
-        document.getElementById('correct').innerHTML += " " + key;
+
+  function userInput(evt) {
+    key = event.key.toLowerCase();
+    // compare input to hangman word
+    checkWord();
+    function checkWord() {
+      let currentLetter = words[0].search(key);
+      let strLength = words[0].length;
+      console.log(currentLetter);
+      //    if wrong
+      if (currentLetter < 0 && guessRight < strLength && lives > 1) {
+        document.getElementById("incorrect").innerHTML += " " + key;
+        lives -= 1;
+        document.getElementById("guessLeft").innerHTML = lives;
+        //    if right
+      } else if (currentLetter >= 0 && guessRight < strLength && lives > 1) {
+        guessRight += 1;
+        if (guessRight == strLength && lives > 1) {
+          document.getElementById("correct").innerHTML += " " + key;
+          wordsGuessed += 1;
+          document.getElementById("gameWin").innerHTML =
+            "You won! Press spacebar to play next round.";
+        } else {
+          document.getElementById("correct").innerHTML += " " + key;
+        }
+      } else {
+        document.getElementById("guessLeft").innerHTML = 0;
+        document.getElementById("gameLose").innerHTML = "Game Over!";
+      }
     }
   }
-}
 
-// if correct
-// store letter guessed in variable
-// add 1 to score
-// add 1 to key counter
-// add 1 to guesses correct
-// output letter to corresponding portion of the page
-// if incorrect
-// store letter in guessed variable
-// add 1 to key counter
-// add 1 to guesses incorrect
-// subtract incorrect guesses from total guesses allowed
-// as long as total guesses > 0, game continues
-// if total guesses = 0, game over
-// output letter to corresponding portion of the page
-//
